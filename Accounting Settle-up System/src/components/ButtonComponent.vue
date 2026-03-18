c245f2e251ebf77cdbc909762de695c22a13fec5
NathanLombardDev <141257106+NathanLombardDev@users.noreply.github.com>
2026-03-18 21:22:15 +0200

    Updated file structure and added scss

---
+++ Accounting Settle-up System/src/components/ButtonComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { AuthService } from '@/services/authService';
+</script>
+
+<template>
+  <button class="btn" @click="AuthService.login">Click Me</button>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ Accounting Settle-up System/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import ButtonComponent from '@/components/ButtonComponent.vue'
+</script>
+
+<template>
+  <div class="login-class">
+    <h1 style="margin-bottom: 2rem;">Login View</h1>
+    <ButtonComponent />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
